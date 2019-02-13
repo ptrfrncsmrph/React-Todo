c924a6c6d7cf0d010064e5b041f97233ff040b94
Peter Murphy <peter.francis.murphy@gmail.com>
2019-02-13 15:03:17 -0500

    Initial commit on Todo project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react"
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -9,8 +9,8 @@ class App extends React.Component {
       <div>
         <h2>Welcome to your Todo App!</h2>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
